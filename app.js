17c2ff9af67de65b5988146fb68a868fc9df9703
melander2910 <58288414+melander2910@users.noreply.github.com>
2021-05-11 14:28:52 +0200

    Go get it

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static('public'));
+
+io.on("connection", (socket) => {
+    console.log("A socket connected with id", socket.id);
+
+    // Welcome to a single client
+    socket.emit("message", "Welcome to heaven")
+
+    // To all clients except the connecting user
+    socket.broadcast.emit("message", "A user has joined the chat");
+        
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat")
+    })
+
+    // changes the color for ALL the sockets EXCEPT itself
+    // socket.broadcast.emit("changeBackgroundToThisColor", data);
+});
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get("/chat", (req, res) => {
+    res.sendFile(__dirname + "/public/chat.html");
+})
+
+server.listen(8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080);
+});
