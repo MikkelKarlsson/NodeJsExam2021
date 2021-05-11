eb36d973302cae2552182495f6de221cd703deb7
melander2910 <58288414+melander2910@users.noreply.github.com>
2021-05-11 18:00:56 +0200

    grim styling samt scrolldown på beskeder

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ const socket = io();
 socket.on("message", message => {
     console.log(message);
     outputMessage(message)
+
+    // scroll down
+    document.getElementById("c-messages").scrollTop = document.getElementById("c-messages").scrollHeight;
 })
 
 // submit user messages
@@ -17,13 +20,21 @@ chatForm.addEventListener("submit", (event) => {
 
     document.getElementById("msg").value = "";
     document.getElementById("msg").focus();
-    
+
 })
 
 // send message to DOM
-function outputMessage(message){
+function outputMessage(message) {
     const div = document.createElement("div");
     div.classList.add("message");
-    div.innerHTML = message;
+    div.innerHTML = `<span class="textbox">${message}</span>`;
     document.getElementById("c-messages").appendChild(div)
 }
+
+// prompt leave room btn
+document.getElementById('leave-btn').addEventListener('click', () => {
+    const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+    if (leaveRoom) {
+        window.location = '../index.html';
+    } else {}
+});
